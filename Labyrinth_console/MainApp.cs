d46499f0eb686f3d36a39c318585db7cc1e4d428
Laura_Echis <laura.echis@gmail.com>
2018-03-27 18:57:19 +0400

    Добавлен main

---
+++ Labyrinth_console/MainApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth_console
+{
+    class MainApp
+    {
+        StreamReader reader = new StreamReader("C:/Users/laura/source/repos/Labyrinth/small-test.in.txt");
+    }
+}
