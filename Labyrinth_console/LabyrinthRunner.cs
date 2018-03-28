34715d11b8b8cd5ecc70727ae7940f76f80bfea8
Laura_Echis <laura.echis@gmail.com>
2018-03-28 19:44:59 +0400

    Коммит от Елены Просвириной Закончена работа над классами LabyrinthRunner и MainApp Кажется, все работает Но это не точно

--- Labyrinth_console/LabyrinthRunner.cs
+++ Labyrinth_console/LabyrinthRunner.cs
@@ -23,21 +23,69 @@ namespace Labyrinth
 
         public void Generate(string firstFlank, string secondFlank)
         {
-            
+            this.countW = firstFlank.Count(x => x == 'W') + secondFlank.Count(x => x == 'W');
+            lab = new Labyrinth(this.countW);
+            playerVector = new Vector(0, 1);
+            currentCoord = new Vector(lab.size / 2, 0);
+            RunnerGenerator(firstFlank.Substring(1));
+            playerVector = exitVector;
+            currentCoord = exitCoord;
+            RunnerGenerator(secondFlank);
         }
 
         public void RunnerGenerator(string flank)
         {
-            
+            while (flank.Length > 0)
+            {
+                var isRotation = false;
+                var reversedVector = new Vector(-playerVector.X, -playerVector.Y);
+                CLoseSideByDirection(lab.arr[currentCoord.Y, currentCoord.X], reversedVector, newValue: true);
+
+                while (flank[0] != 'W')
+                {
+                    isRotation = true;
+                    switch (flank[0])
+                    {
+                        case 'R':
+                            RotateCellRight(lab.arr[currentCoord.Y, currentCoord.X], playerVector);
+                            playerVector.ToTheRight();
+                            break;
+                        case 'L':
+                            RotateCellLeft(lab.arr[currentCoord.Y, currentCoord.X], playerVector);
+                            playerVector.ToTheLeft();
+                            break;
+                    }
+                    flank = flank.Substring(1);
+                }
+
+                if (!isRotation)
+                {
+                    RotateCellForward(lab.arr[currentCoord.Y, currentCoord.X], playerVector);
+                }
+
+                currentCoord.X = currentCoord.X + playerVector.X;
+                currentCoord.Y = currentCoord.Y + playerVector.Y;
+
+                if (flank.Length == 1)
+                {
+                    exitCoord = new Vector(currentCoord.X, currentCoord.Y);
+                    exitVector = new Vector(-playerVector.X, -playerVector.Y);
+                    break;
+                }
+
+                flank = flank.Substring(1);
+            }
         }
+
         public string GetLabyrinthEncryption()
         {
-            return "";
+            return lab.Encrypting_Cells();
         }
 
         private void RotateCellRight(Cell cell, Vector directionVector)
         {
-
+            CLoseSideByDirection(cell, directionVector);
+            RotateCellForward(cell, directionVector);
         }
 
         private void RotateCellLeft(Cell cell, Vector directionVector)
@@ -46,12 +94,21 @@ namespace Labyrinth
 
         private void RotateCellForward(Cell cell, Vector directionVector)
         {
-            
+            var bufVector = new Vector(directionVector.X, directionVector.Y);
+            bufVector.ToTheLeft();
+            CLoseSideByDirection(cell, bufVector);
         }
 
         private void CLoseSideByDirection(Cell cell, Vector directionVector, bool newValue = false)
         {
-
+            if (directionVector.Y == 1 && directionVector.X == 0)
+                cell.bottomWall = newValue;
+            else if (directionVector.Y == -1 && directionVector.X == 0)
+                cell.topWall = newValue;
+            else if (directionVector.Y == 0 && directionVector.X == 1)
+                cell.rightWall = newValue;
+            else if (directionVector.Y == 0 && directionVector.X == -1)
+                cell.leftWall = newValue;
         }
     }
 }
--- Labyrinth_console/MainApp.cs
+++ Labyrinth_console/MainApp.cs
@@ -14,7 +14,25 @@ namespace Labyrinth
 
         public void Run()
         {
-           
+            string[] splitter = { "\r\n" };
+            int string_res = int.Parse(reader.ReadLine());
+            string input_string = "";
+            string output_string = "";
+            //var res = reader.ReadToEnd().Split(splitN, couStrFile, StringSplitOptions.RemoveEmptyEntries);
+            //Console.WriteLine(couStrFile);
+            //for (int j = 1; j < couStrFile; j++)
+            //{
+            //    enter = res[j].Split(' ')[0];
+            //    exit = res[j].Split(' ')[1];
+            //    LabyrinthRunner builder = new LabyrinthRunner();
+            //    builder.Generate(enter, exit);
+            //    Console.WriteLine(builder.GetLabyrinthEncryption());
+            //}
+            LabyrinthRunner builder = new LabyrinthRunner();
+            builder.Generate("WRWWLWWLWWLWLWRRWRWWWRWWRWLW", "WWRRWLWLWWLWWLWWRWWRWWLW");
+            var res = builder.GetLabyrinthEncryption();
+            Console.WriteLine(res);
+            Console.WriteLine("Нажмите Enter, чтобы завершить...");
             Console.ReadLine();
         }
     }
